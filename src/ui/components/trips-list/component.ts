caf5d54cca5ce7248657cedd51466e3a650edaa9
progand <lach@ukr.net>
2017-10-12 16:42:00 +0300

    [+] REnder homepage

--- src/ui/components/trips-list/component.ts
+++ src/ui/components/trips-list/component.ts
@@ -12,7 +12,32 @@ export default class TripsList extends Component {
         this.isLoading = true;
         const res = await fetch(`https://cn.outingtravel.com/models/trips?action=includeRelationships`);
         const data = await res.json();
-        this.trips = data.trips;
+        this.trips = this.deserialize(data);
         this.isLoading = false;
+    }
+
+    deserialize(data: any) {
+        const { trips, photos, users } = data;
+        const result = trips.map(item => {
+            const organiserData = this.getByValue(users, item.organiser);
+            const organiser = {
+                ...organiserData,
+                photo: this.getByValue(photos, organiserData.photo),
+                photos: organiserData.photos.map(photoId => this.getByValue(photos, photoId))
+            }
+            const trip = {
+                ...item,
+                coverPhoto: this.getByValue(photos, item.coverPhoto),
+                photos: item.photos.map(photoId => this.getByValue(photos, photoId)),
+                organiser
+            };
+            return trip;
+        });
+
+        return result;
+    }
+
+    getByValue(collection, fieldValue, fieldName = 'id') {
+        return collection.find(item => item[fieldName] === fieldValue);
     }
 };
--- src/ui/components/trips-list/trip-item/component.ts
+++ src/ui/components/trips-list/trip-item/component.ts
@@ -1,4 +1,22 @@
 import Component from '@glimmer/component';
 
-export default class TripItem extends Component {
+export default class TripItem extends Component {    
+    get dateInterval() {
+        const start = new Date(this.args.trip.dateStart);
+        const end = new Date(this.args.trip.dateEnd);
+
+        return `${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
+    }
+
+    get coverPhotoURL(){
+        return this.getPhotoURL(this.args.trip.coverPhoto, 'default');
+    }
+
+    get organiserPhotoURL(){
+        return this.getPhotoURL(this.args.trip.organiser.photo, 'default');
+    }
+
+    getPhotoURL(photo, size){
+        return photo.sizes && photo.sizes[size] || photo.url;
+    }
 };
